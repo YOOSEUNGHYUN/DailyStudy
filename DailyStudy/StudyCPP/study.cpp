d3a2820b2cb4c190e491eb4edf06241493f3f80a
SEUNGHYUN <ysh7733@naver.com>
2022-01-22 17:51:32 +0900

    studying enumerated types

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -1,15 +1,39 @@
-//studying std::string
+//studying enumerated types
 #include <iostream>
+#include <typeinfo>
 #include <string>
-#include <limits>
 
 using namespace std;
 
+enum Color //user-defined data types
+{
+	COLOR_BLACK,
+	COLOR_RED,
+	COLOR_BLUE,
+	COLOR_GREEN,
+	BLUE,
+
+}; //중괄호가 끝나면 ; 꼭 쳐야한다.
+
 int main()
 {
-	string a("Hello, World");
-	
-	cout << a.length() << endl;
+	int color_id = COLOR_RED;
+
+	Color my_color = static_cast <Color>(3);
+
+	//cin >> my_color;
+
+	//int in_number;
+	//cin >> in_number;
+
+	string str_input;
+
+	getline(cin, str_input);
+
+	if (str_input == "COLOR_BLACK")my_color = static_cast <Color>(0);
+	//...
+
+	cout << my_color << endl;
 
 	return 0;
 }
