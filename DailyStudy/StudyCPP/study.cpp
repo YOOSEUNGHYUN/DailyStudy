ce77240697ee986de2c8cc8b68fb4d8100e4978f
SEUNGHYUN <ysh7733@naver.com>
2022-01-22 15:15:26 +0900

    studying string length

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -7,20 +7,9 @@ using namespace std;
 
 int main()
 {
-	cout << "Your age ? : ";
-	int age;
-	cin >> age;
-	//std::getline(std::cin, age);
-
-	//std::cin.ignore(32767, '\n');
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-	cout << "Your name ? : ";
-	string name;
-	//cin >> name;
-	std::getline(std::cin, name);
-
-	cout << name << " " << age << endl;
+	string a("Hello, World");
+	
+	cout << a.length() << endl;
 
 	return 0;
 }
