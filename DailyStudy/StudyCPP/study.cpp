953285cc738d8a61520fafb65050b1144887ef21
SEUNGHYUN <ysh7733@naver.com>
2022-02-01 15:18:57 +0900

    //studying goto

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -1,34 +1,24 @@
-//studying switch-case
+//studying goto
+
 #include <iostream>
+#include <cmath> //sqrt()
 
 using namespace std;
 
 int main()
 {
-	int x;
+	double x;
+
+	tryAgain : //label 책갈피처럼 쓴다.
+
+	cout << "Enter a non-negative number" << endl; //음수가 아닌 숫자를 입력해라
 	cin >> x;
 
-	switch (x)
-	{
-		case 0:
-		{
-			int y = 5;
-			y = y + x;
-			cout << y << endl;
-			break;
-		}
+	if (x < 0.0)
+		goto tryAgain;
 
-		case 1:
-		{
-			int y = 5;
-			y = y - x;
-			cout << y << endl;
-			break;
-		}
-
-		default: //case에 정의되지않은 모든 경우에 대해서 실행
-			cout << "Undefined input" << x << endl;
-	}
+	cout << sqrt(x) << endl;
 
 	return 0;
+
 }
