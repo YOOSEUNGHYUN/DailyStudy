41c2bb8b82bcbf480f7fbaf19e2d550435f9eab7
SEUNGHYUN <ysh7733@naver.com>
2022-02-01 16:31:22 +0900

    //studying while

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -1,24 +1,26 @@
-//studying goto
+//studying while
 
 #include <iostream>
-#include <cmath> //sqrt()
 
 using namespace std;
 
 int main()
 {
-	double x;
+	cout << "While-loop test" << endl;
 
-	tryAgain : //label 책갈피처럼 쓴다.
+	int outer_count = 1;
+	
+	while (outer_count <= 5)
+	{
+		int inner_count = 1;
+		while (inner_count <= outer_count)
+		{
+			cout << inner_count++ << " ";
+		}
 
-	cout << "Enter a non-negative number" << endl; //음수가 아닌 숫자를 입력해라
-	cin >> x;
-
-	if (x < 0.0)
-		goto tryAgain;
-
-	cout << sqrt(x) << endl;
+		cout << endl;
+		++outer_count;
+	}
 
 	return 0;
-
 }
