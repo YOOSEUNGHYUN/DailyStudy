eb75b1fc3ce346efea763f60d8a3a1f67856a410
SEUNGHYUN <ysh7733@naver.com>
2022-04-29 13:43:01 +0900

    studying random number generation

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -1,23 +1,19 @@
-//studying break,continue
+//studying random number generation
 #include <iostream>
+#include <cstdlib> // std::rand(), std::srand()
+#include <ctime> // std::time()
+#include <random>
 
 using namespace std;
 
 int main()
 {
-	int count(0);
-	
-	while (true)
-	{
-		char ch;
-		cin >> ch;
+	std::random_device rd;
+	std::mt19937 mersenne(rd()); //create a mersenne twister
+	std::uniform_int_distribution<> dice(1, 6); 
 
-		if (ch == 'x')
-			break;
-
-		cout << ch << " " << count++ << endl;
-		
-	}
+	for (int count = 1; count <= 20; ++count)
+		cout << dice(mersenne) << endl;
 
 	return 0;
 }
