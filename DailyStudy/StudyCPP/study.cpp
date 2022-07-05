7ac29e8baa48e206e13111dbb4b54bbbc3643061
SEUNGHYUN <ysh7733@naver.com>
2022-07-05 11:26:22 +0900

    practice Multidimensional Arrays

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 int main()
 {
-	const int num_rows = 3;
-	const int num_columns = 5;
+	const int num_rows = 10;
+	const int num_columns = 10;
 
-	int array[5][4][3];
+	int product[num_rows][num_columns] = { 0 };
 
+	for (int row = 0; row < num_rows; ++row)
+		for (int col = 0; col < num_columns; ++col)
+			product[row][col] = row * col;
 
-	//int array[num_rows][num_columns] = { 0 };
+	for (int row = 1; row < num_rows; ++row)
+	{
+		for (int col = 1; col < num_columns; ++col)
+			cout << product[row][col] << '\t';
+		cout << endl;
 
-	//for (int row = 0; row < num_rows; ++row)
-	//{
-	//	for (int col = 0; col < num_columns; ++col)
-	//		//cout << '[' << row << ']' << '[' << col << ']' << '\t';
-	//		cout << array[row][col] << '\t';
-	//		//cout << (int)&array[row][col] << '\t';
-
-	//	cout << endl;
-	//}
+	}
+	
 	return 0;
 }
 
