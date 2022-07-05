014e05e9c1f7eb8bfce5825aa58f986d113d05d7
SEUNGHYUN <ysh7733@naver.com>
2022-07-06 00:06:33 +0900

    studying C-style strings

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -1,26 +1,22 @@
-//studying Multidimensional Arrays
+//studying C-style strings
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main()
 {
-	const int num_rows = 10;
-	const int num_columns = 10;
+	char source[] = "Copy this!";
+	char dest[50]; 
+	strcpy_s(dest, 50, source);
 
-	int product[num_rows][num_columns] = { 0 };
+	//strcat()
+	strcmp(dest, source);
 
-	for (int row = 0; row < num_rows; ++row)
-		for (int col = 0; col < num_columns; ++col)
-			product[row][col] = row * col;
-
-	for (int row = 1; row < num_rows; ++row)
+	if (strcmp(dest, source) == 0)
 	{
-		for (int col = 1; col < num_columns; ++col)
-			cout << product[row][col] << '\t';
-		cout << endl;
-
+		cout << " same " << endl;
 	}
-	
+
 	return 0;
 }
 
