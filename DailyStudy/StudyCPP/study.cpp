488fd751ba2ba22cf6edebe79d81a342ca64fc76
SEUNGHYUN <ysh7733@naver.com>
2022-07-09 17:21:25 +0900

    studying pointer and array

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -1,42 +1,29 @@
-//studying Null pointer
+//studying pointer and array
 #include <iostream>
-#include <cstddef>
-
 using namespace std;
 
-void doSomething(double *ptr)
+//= void printArray(int array[])
+void printArray(int *array)
 {
-	cout << " Address of pointer variable in doSomething() " << &ptr << endl;
-
-	if (ptr != nullptr) 
-	{
-		//do something useful
-		cout << *ptr << endl;
-	}
-	else
-	{
-		//do nothing with ptr
-		cout << "Null ptr, do nothing" << endl;
-	}
+	cout << sizeof(array) << endl;
+	cout << "de-referncing : " << *array << endl;
+	*array = 100;
 }
 
 int main()
 {
-	double *ptr{ nullptr }; //Modern C++ style 
+	int array[5] = { 9, 7, 5, 3, 1 };
 
-	doSomething(ptr);
-	doSomething(nullptr);
+	cout << sizeof(array) << endl; //20
+	
+	int *ptr = array;
 
-	double d = 123.4;
+	cout << sizeof(ptr) << endl; //4 (pointer)
+	        
+	printArray(array);
 
-	doSomething(&d);
-
-	ptr = &d;
-
-	doSomething(ptr);
-
-	cout << " Address of pointer variable in main() " << &ptr << endl;
+	cout << array[0] <<" " << *array << endl;
 
 	return 0;
 
-}
+}    
