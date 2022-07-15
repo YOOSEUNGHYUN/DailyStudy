25cfbfebc5abdc0efb63b7b3c1aaae9d0568f36f
SEUNGHYUN <ysh7733@naver.com>
2022-07-15 22:04:12 +0900

    studying pointer and const

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -1,26 +1,14 @@
-//studying Dynamically allocating arrays
+//studying pointer and const
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int length = 5;
-
-	cin >> length;
-
-	//int array[length];
-	int* array = new int[length] {11,22,33,44,55,66};
-
-	array[0] = 1;
-	array[1] = 2;
-
-	for (int i = 0; i < length; ++i)
-	{
-		cout << (uintptr_t)&array[i] << endl;
-		cout << array[i] << endl;
-	}
-
-	delete [] array;
-
+	int value = 5;
+	const int *ptr1 = &value;
+	int *const ptr2 = &value;
+	const int *const ptr3 = &value;
+	
 	return 0;
 }
+
