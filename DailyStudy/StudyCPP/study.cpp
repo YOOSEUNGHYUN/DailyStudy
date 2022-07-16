4693691a2a696fa182f9ff6cbaea23f95402f422
SEUNGHYUN <ysh7733@naver.com>
2022-07-16 23:02:33 +0900

    studying Reference Variable2

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -2,19 +2,23 @@
 #include <iostream>
 using namespace std;
 
+
+void printElements(int (&arr)[5])
+{
+	for (int i = 0; i < 5; ++i)
+	{
+		cout << arr[i] << endl;
+	}
+	
+	cout << endl;
+}
+
 int main()
 {
-	int value = 5;
+	const int length = 5;
+	int arr[length] = { 1,2,3,4,5 };
 
-	int* ptr = nullptr;
-	ptr = &value;
-	
-	int &ref = value;
-
-	cout << "value : " << &value << endl;
-	cout << "ref : " << &ref << endl;
-	cout << "ptr : " << ptr << endl;
-	cout << "&ptr : " << &ptr << endl;
+	printElements(arr);
 
 	return 0;
 }
