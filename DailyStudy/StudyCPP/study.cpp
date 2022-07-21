cb66b499f30e27c593afe6865c739ff1d8c2bd6a
SEUNGHYUN <ysh7733@naver.com>
2022-07-21 23:45:37 +0900

    studying Pointers to pointers and dynamic multidimensional arrays 3

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -15,33 +15,49 @@ int main()
 		{11,12,13,14,15}
 	};
 
-	int **matrix = new int* [row];
-
-	for(int r = 0; r < row; ++r)
-		matrix[r] = new int[col];
+	int *matrix = new int[row*col];
 
 	for (int r = 0; r < row; ++r)
 		for (int c = 0; c < col; ++c)
-			matrix[r][c] = s2da[r][c]; //matrix에 값을 대입하는 부분을 넣어줌
+			matrix[c + col*r] = s2da[r][c];
 
 	// print all elements
 	for (int r = 0; r < row; ++r)
 	{
 		for (int c = 0; c < col; ++c)
-			cout << matrix[r][c] << " "; //이게 matrix 자체다
-		cout << endl;//줄이(row가) 바뀔 때마다 endl; 하나씩 넣어줌
+			cout << matrix[c + col*r] << " "; 
+		cout << endl;
 	}
-
-	// delete
-	for (int r = 0; r < row; ++r)
-		delete [] matrix[r];
 
 	delete[] matrix;
 
-	/*delete[] r1;
-	delete[] r2;
-	delete[] r3;
-	delete[] matrix;*/
+	//int **matrix = new int* [row];
+
+	//for(int r = 0; r < row; ++r)
+	//	matrix[r] = new int[col];
+
+	//for (int r = 0; r < row; ++r)
+	//	for (int c = 0; c < col; ++c)
+	//		matrix[r][c] = s2da[r][c]; //matrix에 값을 대입하는 부분을 넣어줌
+
+	//// print all elements
+	//for (int r = 0; r < row; ++r)
+	//{
+	//	for (int c = 0; c < col; ++c)
+	//		cout << matrix[r][c] << " "; //이게 matrix 자체다
+	//	cout << endl;//줄이(row가) 바뀔 때마다 endl; 하나씩 넣어줌
+	//}
+
+	//// delete
+	//for (int r = 0; r < row; ++r)
+	//	delete [] matrix[r];
+
+	//delete[] matrix;
+
+	///*delete[] r1;
+	//delete[] r2;
+	//delete[] r3;
+	//delete[] matrix;*/
 
 	return 0;
 
