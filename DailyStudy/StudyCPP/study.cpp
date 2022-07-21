e42c7f00f8e4bb1b9c5b83ff69a8c3f620fbfd0d
SEUNGHYUN <ysh7733@naver.com>
2022-07-21 23:16:33 +0900

    studying Pointers to pointers and dynamic multidimensional arrays2

--- DailyStudy/StudyCPP/study.cpp
+++ DailyStudy/StudyCPP/study.cpp
@@ -5,19 +5,43 @@ using namespace std;
 
 int main()
 {
-	int *ptr = nullptr;
-	int **ptrptr = nullptr;
+	const int row = 3;
+	const int col = 5;
 
-	int value = 5;
+	const int s2da[row][col] =
+	{
+		{1,2,3,4,5},
+		{6,7,8,9,10},
+		{11,12,13,14,15}
+	};
 
-	ptr = &value;
-	ptrptr = &ptr;
+	int **matrix = new int* [row];
 
-	cout << ptr << " " << *ptr << " " << &ptr << endl;
-	cout << ptrptr << " " << *ptrptr << " " << &ptrptr << endl;
-	cout << *(*ptrptr) << endl;
+	for(int r = 0; r < row; ++r)
+		matrix[r] = new int[col];
 
-	int ***ppptr, ****pppptr;
+	for (int r = 0; r < row; ++r)
+		for (int c = 0; c < col; ++c)
+			matrix[r][c] = s2da[r][c]; //matrix에 값을 대입하는 부분을 넣어줌
+
+	// print all elements
+	for (int r = 0; r < row; ++r)
+	{
+		for (int c = 0; c < col; ++c)
+			cout << matrix[r][c] << " "; //이게 matrix 자체다
+		cout << endl;//줄이(row가) 바뀔 때마다 endl; 하나씩 넣어줌
+	}
+
+	// delete
+	for (int r = 0; r < row; ++r)
+		delete [] matrix[r];
+
+	delete[] matrix;
+
+	/*delete[] r1;
+	delete[] r2;
+	delete[] r3;
+	delete[] matrix;*/
 
 	return 0;
 
